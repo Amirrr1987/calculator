f81ccb9c726eba95f6a91fb9087068dbed31435d
Amirrr1987 <Maghami.A1987@gmail.com>
2020-09-19 14:34:26 +0430

    add style in App.vue

--- src/App.vue
+++ src/App.vue
@@ -28,3 +28,132 @@ export default {
 
 }
 </script>
+
+<style lang="scss">
+@function height($size: $width){
+  @return $size * 1.7;
+}
+$width: 15em;
+$main-color: #60627B;
+button{
+  cursor: pointer;
+  border: none;
+  outline: none;
+  font-size: 1.2em;
+  border:none;
+  &:active{
+      border-radius: 4px;
+      box-shadow: 3px 3px 0px $main-color;
+      color: orange;
+  }
+}
+.app{
+  background-color: darken($main-color , 2%);
+  width : $width;
+  height: height($width);  
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+}
+.calculator {
+  height: 90%;
+  width : 90%;
+  background-color: $main-color;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: repeat(7,1fr);
+  grid-template-areas:
+    'answer answer answer answer'
+    'answer answer answer answer'
+    'clean division multiplication minus'
+    'n7 n8 n9 plus'
+    'n4 n5 n6 equal'
+    'n1 n2 n3 equal'
+    'n0 n0 decimal equal'
+    ;
+  gap: 13px;
+    &__item {
+      background-color: $main-color;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: #FFF;
+      box-shadow: 3px 3px 11px #1e1f20, -2px -2px 10px rgb(151,147,147);
+    }
+
+    &--answer {
+      grid-area: answer;
+      background-color: lighten($main-color, 10%);
+      box-shadow: inset 0px 0px 4px #1e1f20, -2px -2px 10px #979393;
+    }
+
+    &--clean {
+      grid-area: clean;
+   }
+
+    &--decimal {
+      grid-area: decimal;
+   }
+
+    &--division {
+      grid-area: division;
+    }
+
+    &--equal {
+      grid-area: equal;
+   }
+
+    &--minus {
+      grid-area: minus;
+    }
+
+    &--multiplication {
+      grid-area: multiplication;
+    }
+
+    &--n0 {
+      grid-area: n0;
+    }
+  
+    &--n1 {
+      grid-area: n1;
+    }
+
+    &--n2 {
+      grid-area: n2;
+    }
+
+    &--n3 {
+      grid-area: n3;
+    }
+
+    &--n4 {
+      grid-area: n4;
+    }
+
+    &--n5 {
+      grid-area: n5;
+    }
+
+    &--n6 {
+      grid-area: n6;
+    }
+
+    &--n7 {
+      grid-area: n7;
+    }
+
+    &--n8 {
+      grid-area: n8;
+    }
+
+    &--n9 {
+      grid-area: n9;
+    }
+
+    &--plus {
+      grid-area: plus;
+    }
+}
+</style>

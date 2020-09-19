8cb758a8d77a09e05c44cb534aa73355972a5c58
Amirrr1987 <Maghami.A1987@gmail.com>
2020-09-19 14:31:23 +0430

    add app tags width css BEM class

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,30 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <body id="app">
+    <div class="calculator">
+    <div class="calculator__item calculator--answer"></div>
+    <button class="calculator__item calculator--clean">C</button>
+    <button class="calculator__item calculator--division">/</button>
+    <button class="calculator__item calculator--multiplication">*</button>
+    <button class="calculator__item calculator--minus">-</button>
+    <button class="calculator__item calculator--plus">+</button>
+    <button class="calculator__item calculator--equal">=</button>
+    <button class="calculator__item calculator--decimal">.</button>
+    <button class="calculator__item calculator--n0">0</button>
+    <button class="calculator__item calculator--n1">1</button>
+    <button class="calculator__item calculator--n2">2</button>
+    <button class="calculator__item calculator--n3">3</button>
+    <button class="calculator__item calculator--n4">4</button>
+    <button class="calculator__item calculator--n5">5</button>
+    <button class="calculator__item calculator--n6">6</button>
+    <button class="calculator__item calculator--n7">7</button>
+    <button class="calculator__item calculator--n8">8</button>
+    <button class="calculator__item calculator--n9">9</button>
   </div>
+  </body>
 </template>
+<script>
+export default {
+  name: 'App',
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</script>
